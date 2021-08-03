aa03a1ea01265bf6e05547d249655cf9881967ae
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2021-08-02 21:33:30 -0500

    Updated SHIT CUS I WANT TO DIe

--- Invmenu test/src/IcyEndymion004/main.php
+++ Invmenu test/src/IcyEndymion004/main.php
@@ -3,14 +3,49 @@
 namespace icyEndymion004;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\server;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use muqsit\invmenu\InvMenuHandler;
+muqsit\invmenu\InvMenu;
+
 
 class main extends PluginBase{ 
 
     public function onLoad(){
         $this->getLogger()->info("Someone FUCKING KILL ME");
+
       }
+      public function onEnable() : void{
+		if(!InvMenuHandler::isRegistered()){
+			InvMenuHandler::register($this);
+		}
+	}
     
       public function onDisable(){
         $this->getLogger()->info("I WANT TO DIE");
       }
+      public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+        switch($cmd->getName()){
+          case "test":
+            if(!$sender instanceof Player){ 
+              $sender->sendMessage("FUCK OFF"); 
+            }else{ 
+                $menu = InvMenu::create(InvMenu::TYPE_CHEST);
+                $menu->setListener(function(InvMenuTransaction $transaction) : InvMenuTransactionResult{
+	            $player = $transaction->getPlayer();
+	            $action = $transaction->getAction();
+	            $player->removeWindow($action->getInventory());
+	            return $transaction->discard()->then(function(Player $player) : void{
+		        $player->sendForm(new class() implements Form{});
+	            });
+                });
+            }
+          break;
+        }
+        return true;
+      }
+     
 }
