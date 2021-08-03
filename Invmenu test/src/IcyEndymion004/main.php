b640e12f5a54f97c660d3a5062f7b72d8cc74676
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2021-08-02 21:28:49 -0500

    Add files via upload

---
+++ Invmenu test/src/IcyEndymion004/main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace icyEndymion004;
+
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase{ 
+
+    public function onLoad(){
+        $this->getLogger()->info("Someone FUCKING KILL ME");
+      }
+    
+      public function onDisable(){
+        $this->getLogger()->info("I WANT TO DIE");
+      }
+}
